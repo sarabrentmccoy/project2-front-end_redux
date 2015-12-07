c4d0ef0d9f3c245d2d44aa482b0c7f7a0c0ad94a
sarabrent <sarabrentmccoy@gmail.com>
2015-12-07 15:37:28 -0500

    Adds sites-show click handler.

--- js/navigation.js
+++ js/navigation.js
@@ -1,4 +1,11 @@
 $('#login-reg-show').click(function(){
 	$('#login-reg').show();
 	$('#main').hide();
+	$('#sites').hide();
 });
+
+$('#sites-show').click(function(){
+	$('#sites').show();
+	$('#main').hide();
+	$('login-reg').hide();
+})
