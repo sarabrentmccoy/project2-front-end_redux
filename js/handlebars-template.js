7782e57b5a705d956555a0eb049067a7a0fa7093
sarabrent <sarabrentmccoy@gmail.com>
2015-12-08 16:03:39 -0500

    Hides and shows pages upon clicking on all-sites-show

--- js/handlebars-template.js
+++ js/handlebars-template.js
@@ -1,22 +1,12 @@
+
 var siteIndexTemplate = Handlebars.compile($('#site-index').html());
 
-$('#all-sites').click(function(e){
-  $.ajax({
-    method: "GET",
-    url: "https://localhost:3000",
-    dataType: "json"
-  }).done(function(data){
-    var siteIndexTemplate = Handlebars.compile($('#site-index').html());
-    var siteHTML = siteIndexTemplate({site:data});
-    $('#populate-sites').html('');
-    $('#populate-sites').append(siteHTML);
-  }).fail(function(data){
-    console.error(data);
-  });
-});
-
-
-$('#minimal').click(function(e){
+$('#all-sites-show').click(function(e){
+  $('#all-sites').show();
+  $('#my-sites').hide();
+  $('#main').hide();
+  $('#login-reg').hide();
+  $('#create-sites').hide();
   $.ajax({
     method: "GET",
     url: "https://localhost:3000",
