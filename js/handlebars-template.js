b5a7c8f892fbc3464b3d28750ad45da13813d441
sarabrent <sarabrentmccoy@gmail.com>
2015-12-09 09:28:15 -0500

    Adds my-sites click function (hiding and showing divs, GETting data to populate handlebars template with) to handlebars template

--- js/handlebars-template.js
+++ js/handlebars-template.js
@@ -20,3 +20,24 @@ $('#all-sites-show').click(function(e){
     console.error(data);
   });
 });
+
+$('#my-sites-show').click(function(e){
+  $('#all-sites').hide();
+  $('#my-sites').show();
+  $('#main').hide();
+  $('#login-reg').hide();
+  $('#create-sites').hide();
+  $.ajax({
+    method: "GET",
+    url: "https://localhost:3000",
+    dataType: "json"
+  }).done(function(data){
+    var siteIndexTemplate = Handlebars.compile($('#site-index').html());
+    var siteHTML = siteIndexTemplate({site:data});
+    $('#populate-sites').html('');
+    $('#populate-sites').append(siteHTML);
+  }).fail(function(data){
+    console.error(data);
+  });
+});
+
